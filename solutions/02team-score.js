3fe8bc17f467d2dd5a53d52bf0ab39e092c76b33
Manuel Almeida <alejandro121791@hotmail.com>
2022-03-21 11:56:26 +0100

    finished-team-score

--- solutions/02team-score.js
+++ solutions/02team-score.js
@@ -6,13 +6,42 @@ const scores = {
     dolphinScore2 : [97, 112, 101],
     koalaScore2 : [109, 95, 123],
     dolphinScore3 : [97, 112, 101],
-    koalaScore3 : [109, 95, 108],
+    koalaScore3 : [109, 95, 106],
 }
 
 //console.log(Object.keys(scores));
 function calcAvg(numbers) {
     const sum = numbers.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
-    return sum / numbers.lenght;
+    return sum / numbers.length;
 }
 
-//for (const key of Object.keys(scores)) {}
+function destructureObjToValues(obj, func) {
+
+    let objValues = {};
+    for (let [key, value] of Object.entries(obj)) {
+        //console.log(`${key}: ${value}`);
+
+        objValues[key] = func(value);
+        
+    }
+    return objValues;
+}
+
+const avgObj = destructureObjToValues(scores, calcAvg)
+
+function compareAvg(dolphins, koalas) {
+    if (dolphins > koalas && dolphins >= 100) {
+        console.log(`Dolphins Won`)
+    } else if (koalas > dolphins && koalas >= 100) {
+        console.log(`Koalas Won`)
+    } else if (koalas == dolphins && koalas >= 100 && dolphins >= 100) {
+        console.log(`It's a draw`)
+    } else {
+        console.log(`No team won`)
+    }
+}
+
+compareAvg(avgObj.dolphinScore1, avgObj.koalaScore1);
+compareAvg(avgObj.dolphinScore2, avgObj.koalaScore2);
+compareAvg(avgObj.dolphinScore3, avgObj.koalaScore3);
+
---
+++ solutions/order-an-array.js
@@ -0,0 +1,5 @@
+p1 = [{firstName:"John", lastName:"Doe", age:46},
+{firstName:"Joanne", lastName:"Doe", age:43},
+{firstName:"Rafael", lastName:"Milanes", age:30},
+{firstName:"Luis", lastName:"Paz", age:33}
+];
