7863ff77759e11daa4a9354215fd9dc9d7c7749d
Manuel Almeida <alejandro121791@hotmail.com>
2022-03-23 15:11:29 +0100

    make-branch

--- solutions/03tipCalculator.js
+++ solutions/03tipCalculator.js
@@ -1,18 +1,31 @@
 
 const bills = [275, 40, 430];
 
-/* function check(bill) {
-    const tip = bill < 50 && bill > 300 ? 15 / 100 * bill : 20 / 100 * bill;
-    const total = tip + bill;
-    return `The bill was ${bill}, the tip was ${tip} and the total money spend was ${total}.`
-}
- */
+
 function calcTip(bill) {
     return bill > 50 && bill < 300 ? 15 / 100 * bill : 20 / 100 * bill;
 }
 
 const totals = [];
 const tips = [];
+
+
+bills.forEach(bill => {
+    const tip = calcTip(bill);
+    tips.push(tip);
+    totals.push(bill + tip);
+});
+
+console.log(tips);
+console.log(totals);
+
+/* function check(bill) {
+    const tip = bill < 50 && bill > 300 ? 15 / 100 * bill : 20 / 100 * bill;
+    const total = tip + bill;
+    return `The bill was ${bill}, the tip was ${tip} and the total money spend was ${total}.`
+}
+ */
+
 //tips.push(calcTip())
 //tips.push(bills.map(el => calcTip(el)));
 /* for (let i = 0; i < bills.length; i++) {
@@ -38,17 +51,4 @@ for (let i = 0; i < bills.length; i++) {
 console.log(calcTipAndTotal(bills));
  */
 
-bills.forEach(bill => {
 
-    const tip = bill > 50 && bill < 300 ? 15 / 100 * bill : 20 / 100 * bill;
-const total = tip + bill;
-console.log(`The bill was ${bill}, the tip was ${tip} and the total money spend was ${total}.`)
-});
-
-    const tip = calcTip(bill);
-    tips.push(tip);
-    totals.push(bill + tip);
-});
-
-console.log(tips);
-console.log(totals);
