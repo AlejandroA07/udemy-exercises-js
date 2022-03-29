58b1a026f6cc5393f2bbd991d45b0b4e84400a4a
Manuel Almeida <alejandro121791@hotmail.com>
2022-03-29 10:13:34 +0200

    Update 04thermometer.js

--- solutions/04thermometer.js
+++ solutions/04thermometer.js
@@ -1,34 +1,18 @@
-const temp = [17, 21, 23, 12, 26, 18, 19, 25, 16, 28, 15, 21, 13, 29, 30, 17, 21, 23, 12, 26, 18, 19, 25, 16, 28, 15, 21, 13, 29, 30];
+const temp = [17, 21, 23, 12, 26, 18, 19, 25, 16, 28, 15, 21, 13, 29, 30, 17, 21, 23, 12, 26, 18, 19, 25, 16, 28, 15, 21, 13, 29, 30, 17, 21, 23, 12, 26, 18, 19, 25, 16, 28, 15, 21, 13, 29, 30, 17, 21, 23, 12, 26, 18, 19, 25, 16, 28, 15, 21, 13, 29, 30, 17, 21, 23, 12, 26, 18, 19, 25, 16, 28, 15, 21, 13, 29, 30, 17, 21, 23, 12, 26, 18, 19, 25, 16, 28, 15, 21, 13, 29, 30, 17, 21, 23, 12, 26, 18, 19, 25, 16, 28, 15, 21, 13, 29, 30, 17, 21, 23, 12, 26, 18, 19, 25, 16, 28, 15, 21, 13, 29, 30, 17, 21, 23, 12, 26, 18, 19, 25, 16, 28, 15, 21, 13, 29, 30, 17, 21, 23, 12, 26, 18, 19, 25, 16, 28, 15, 21, 13, 29, 32];
 const arr = [17, 21, 23];
 
 
-function logOutStringForEach(arr) {
+/* function logOutStringForEach(arr) {
   let b = "";
   arr.forEach((el, index,) => {
       const a = `${el}C the ${ordinal(index + 1)} day\n` ;
       b += a;
   })
   return b;
-} 
+}  */
 
- 
 
-function ordinal(index) {
-  const ind = index.toString();
-const indIndex = ind.charAt(ind.length - 1);
-
-    if (indIndex == 1 && ind.charAt(ind.length -2) != 1) {
-      return index + "st"
-    } else if (indIndex == 2 && ind.charAt(ind.length -2) != 1) {
-      return index + "nd"
-    } else if (indIndex == 3 && ind.charAt(ind.length -2) != 1) {
-      return index + "rd"
-    } else {
-      return index + "th"
-    } 
-}
-
-function logOutString(arr) {
+/* function logOutString(arr) {
   
   let b = "";
   
@@ -42,10 +26,34 @@ function logOutString(arr) {
   }
   return b;
   
+} */
+
+/* function ordinalNumAlmost(index) {
+  const units = Math.trunc(index % 10);
+  const tenths = Math.trunc(Math.trunc(index / 10) % 10);
+
+  if (tenths == 1)
+  return "th";
+  if (units == 1)
+  return "st";
+  if (units == 2)
+  return "nd";
+  if (units == 3)
+  return "rd";
+   
+  return "th";
+} */
+
+function ordinalNumFunctional(index) {
+  const ordinal = ["th", "st", "nd", "rd"]
+  const units = Math.trunc(index % 10);
+  const tenths = Math.trunc(Math.trunc(index / 10) % 10);
+  const n = units > 3 || tenths == 1 ? 0 : units;
+  return ordinal[n];
 }
 
 function logOutStringForEachFunctional(arr) {
-  return arr.map((el, index) => `${el}C the ${ordinal(index + 1)} day`)
+  return arr.map((el, index) => `${el}C the ${(index + 1) + ordinalNumFunctional(index + 1)} day`)
   .join('\n')
 }
 const output = logOutStringForEachFunctional(temp);
