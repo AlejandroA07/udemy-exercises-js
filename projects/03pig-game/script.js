bd9cc9a2b96ed7b0012e1aa5118ef09633aefc3d
Manuel Almeida <alejandro121791@hotmail.com>
2022-04-07 15:28:32 +0200

    added-some-more-functionality

--- projects/03pig-game/script.js
+++ projects/03pig-game/script.js
@@ -3,9 +3,12 @@
 let diceRoll = document.querySelector('.btn--roll');
 let hold = document.querySelector('.btn--hold');
 let currentScoreInDom = document.getElementById('current--0');
-let score1 = document.getElementById('score--0').textContent;
+let player1Score = document.getElementById('score--0').textContent;
+let player1 = document.querySelector('.player--0');
+let player2 = document.querySelector('.player--1');
 let currentScore = 0;
 let activePlayer = 0;
+
 
 
 function rollDice() {
@@ -21,6 +24,7 @@ function rollDice() {
         document.getElementById(`current--${activePlayer}`).textContent = 0;
         //activePlayer = activePlayer === 0 ? 1 : 0;
         switchPlayer()
+        addStyleToActivePlayer()
         currentScore = 0;
     }
 }
@@ -38,8 +42,17 @@ function switchPlayer() {
     return activePlayer;
 };
 
+function addStyleToActivePlayer() {
+    player1.classList.toggle('player--active');
+    player2.classList.toggle('player--active');
+}
+
 function holdenScore() {
     
+    document.getElementById(`score--${activePlayer}`).textContent = Number(document.getElementById(`score--${activePlayer}`).textContent) + currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    switchPlayer();
 }
 
 
