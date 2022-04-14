691e14b6f57a19f1d8a093853577e67a7279807b
Manuel Almeida <alejandro121791@hotmail.com>
2022-04-14 13:27:10 +0200

    added-data

--- challenges/betting-app.js
+++ challenges/betting-app.js
@@ -85,3 +85,20 @@ for (const [team, odd] of Object.entries(game.odds)) {
     const teamstr = team === 'x' ? 'draw' : `victory ${game[team]}`;
     console.log(`Odd of ${teamstr} ${odd}`);
 }
+
+const gameEvents = new Map([
+    [17, '⚽ GOAL'],
+    [36, '🔁 Substitution'],
+    [47, '⚽ GOAL'],
+    [61, '🔁 Substitution'],
+    [64, '🟨 Yellow card'],
+    [69, '🟥 Red card'],
+    [70, '🔁 Substitution'],
+    [72, '🔁 Substitution'],
+    [76, '⚽ GOAL'],
+    [80, '⚽ GOAL'],
+    [92, '🟨 Yellow card'], 
+]);
+
+const events = new Set(gameEvents);
+
